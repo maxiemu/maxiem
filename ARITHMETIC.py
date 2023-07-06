8d085efcb62659fcf4baa147d3563c091687dbe9
maxiemu <138739786+maxiemu@users.noreply.github.com>
2023-07-06 13:30:33 +0800

    Add files via upload

---
+++ ARITHMETIC.py
@@ -0,0 +1,12 @@
+num1 = 5
+num2 = 8
+
+add = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+print("the Sum is:" + str(add))
+print("the Differenct is:" str(sub))
+print("the Product is:" str(mult))
+print("the Quotient is:" str(div))
---
+++ PersonalInformation.py
@@ -0,0 +1,20 @@
+print("Programmer: Ivan Glenn D Sombito")
+print("Date Created: July 6 2023")
+print("Description of Program: Info")
+
+
+print("First Name: Ivan Glenn")
+print("Middle Name: Dinio")
+print("Last Name: Sombito")
+print("Course: BSIT")
+print("Year: 3rd")
+print("Age: 20")
+print("Gender: Male")
+print("Civil Status: Single")
+print(" Nationality: Filipino")
+
+print("Motto: Pag kaya nila, pagawa mo sa kanila")
+
+
+print("School: PHINMA Araullo University")
+print("College: CITE")
