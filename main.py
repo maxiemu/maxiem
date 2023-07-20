f17e3f6dc457ac4c92a257301e12671edb348c15
maxiemu <138739786+maxiemu@users.noreply.github.com>
2023-07-19 22:45:43 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+print("SAS 5")
+print("CONVERSION PROGRAM")
 
-msg = input("Enter a message to encrypt: ")
-encmsg = ""
-for ch in msg:
-    asc = ord(ch) + 3
-    ench = chr(asc)
-    encmsg += ench
-print("Encrypted message:",encmsg)
+meter = int(input("Enter the Meter to Convert:"))
+
+millimeter = meter * 1000
+centimeter = meter * 100
+decimeter = meter * 10
+kilometer = meter / 1000
+inch = meter * 39.37
+foot = meter * 3.28
+mile = meter / 1609.344
+
+print(meter, "Meter to Millimeter", millimeter)
+print(meter, "Meter to Centimeter", centimeter)
+print(meter, "Meter to Decimeter", decimeter)
+print(meter, "Meter to Kilometer", kilometer)
+print(meter, "Meter to Inch", inch)
+print(meter, "Meter to Foot", foot)
+print(meter, "Meter to Mile", mile)
